0500961803c276264e92d6a839f112064ac43156
Tina Nguyen <nguyentinaca@yahoo.com>
2021-03-11 20:16:55 -0800

    display note HTML

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const PORT = process.env.PORT || 3005;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.get( '/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.listen(PORT, () => { 
     console.log(`API server now on port ${PORT}!`);
