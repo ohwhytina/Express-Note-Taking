f22d16807e89e25e712b98c2eef1ac9390705416
Tina Nguyen <nguyentinaca@yahoo.com>
2021-03-13 18:05:59 -0800

    minor fix

--- server.js
+++ server.js
@@ -1,73 +1,61 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { notes } = require("./db/db.json");
 
-const PORT = process.env.PORT || 3006;
+
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-var saveNote = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // pull note.html
-app.get( '/notes', (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
 // show note.html
 app.get("/api/notes", (req, res) => {
-    return res.json(saveNote)
-    
-});
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+})
 
 
 // go back to main page
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 
-
 // save notes! 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
+    var saveNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     var newNote = req.body;
     newNote.id = saveNote.length.toString()
     saveNote.push(newNote);
     
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(saveNote)
-    );
+    fs.writeFileSync("./db/db.json", JSON.stringify(saveNote));
     console.log(saveNote);
-    res.json(saveNote)
+    res.json(saveNote);
 });
 
 // delete notes 
-app.delete('/api/notes/:id', (req, res) => {
-    var noteId = req.params.id;
-    var newId = 0;
+app.delete("/api/notes/:id", (req, res) => {
+    var saveNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    var noteId = req.params.id.toString();
     saveNote = saveNote.filter(List => {
         return List.id != noteId
-    })
-    for (List of saveNote) {
-        List.id = newId.toString();
-        newId++;
-    }
+    });
 
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(saveNote)
-    );
+    fs.writeFileSync("./db/db.json", JSON.stringify(saveNote));
     console.log(saveNote);
     res.json(saveNote)
-})
+});
 
 
 
 app.listen(PORT, () => { 
     console.log(`API server now on port ${PORT}!`);
-})
+});
